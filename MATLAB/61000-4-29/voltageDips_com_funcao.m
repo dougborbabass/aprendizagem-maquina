64e4955bf89ccca3a68d84c25d07868b9fc6f176
Douglas Borba <douglasbeng@gmail.com>
2020-10-29 13:09:55 -0300

    adicionado short interruption no csv

--- MATLAB/61000-4-29/voltageDips_com_funcao.m
+++ MATLAB/61000-4-29/voltageDips_com_funcao.m
@@ -1,35 +1,35 @@
 clc;
 clear;
 
-amostras = 1000;
+amostras = 29001;
 % Número de parametros na tabela
-nQuedas = 20;
+nQuedas = 10;
+nShorts = 7;
 voltageDips = cell(1,nQuedas);
+shortInterruption = cell(1,nShorts);
 
 %% Gerar voltage dips
-voltageDips{1} = gerarDips(3.3,40,0.01,10);
-voltageDips{2} = gerarDips(3.3,40,0.03,10);
-voltageDips{3} = gerarDips(3.3,40,0.1,10);
-voltageDips{4} = gerarDips(3.3,40,0.3,10);
-voltageDips{5} = gerarDips(3.3,40,1,10);
+voltageDips{1} = gerarDips(3.3,40,0.01,3);
+voltageDips{2} = gerarDips(3.3,40,0.03,3);
+voltageDips{3} = gerarDips(3.3,40,0.1,3);
+voltageDips{4} = gerarDips(3.3,40,0.3,3);
+voltageDips{5} = gerarDips(3.3,40,1,3);
 
-voltageDips{6} = gerarDips(3.3,70,0.01,10);
-voltageDips{7} = gerarDips(3.3,70,0.03,10);
-voltageDips{8} = gerarDips(3.3,70,0.1,10);
-voltageDips{9} = gerarDips(3.3,70,0.3,10);
-voltageDips{10} = gerarDips(3.3,70,1,10);
+voltageDips{6} = gerarDips(3.3,70,0.01,3);
+voltageDips{7} = gerarDips(3.3,70,0.03,3);
+voltageDips{8} = gerarDips(3.3,70,0.1,3);
+voltageDips{9} = gerarDips(3.3,70,0.3,3);
+voltageDips{10} = gerarDips(3.3,70,1,3);
 
-% voltageDips{11} = gerarDips(5,40,0.01,10);
-% voltageDips{12} = gerarDips(5,40,0.03,10);
-% voltageDips{13} = gerarDips(5,40,0.1,10);
-% voltageDips{14} = gerarDips(5,40,0.3,10);
-% voltageDips{15} = gerarDips(5,40,1,10);
-% 
-% voltageDips{16} = gerarDips(5,70,0.01,10);
-% voltageDips{17} = gerarDips(5,70,0.03,10);
-% voltageDips{18} = gerarDips(5,70,0.1,10);
-% voltageDips{19} = gerarDips(5,70,0.3,10);
-% voltageDips{20} = gerarDips(5,70,1,10);
+
+%% Gerar Short interruption 
+shortInterruption{1} = gerarDips(3.3,100,0.001,3);
+shortInterruption{2} = gerarDips(3.3,100,0.003,3);
+shortInterruption{3} = gerarDips(3.3,100,0.01,3);
+shortInterruption{4} = gerarDips(3.3,100,0.03,3);
+shortInterruption{5} = gerarDips(3.3,100,0.1,3);
+shortInterruption{6} = gerarDips(3.3,100,0.3,3);
+shortInterruption{7} = gerarDips(3.3,100,1,3);
 
 
 %% Gerar o header
@@ -46,8 +46,8 @@ for i = 2:length(names)
 end
 
 
-%%
-%write the string to a file
+%
+% write the string to a file
 fid = fopen('iec6100429.csv','w');
 fprintf(fid,'%s\r\n',header_string);
 fclose(fid);
@@ -56,4 +56,6 @@ for j = 1:nQuedas
     dlmwrite('iec6100429.csv', voltageDips{j}, 'precision', '%.3f', '-append')
 end
 
-
+for x = 1:nShorts
+    dlmwrite('iec6100429.csv', shortInterruption{x}, 'precision', '%.3f', '-append')
+end
